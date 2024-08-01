78a8a3fa3284f56c57fca972fe3d8b0091791ebe
justin951 <justin951@revature.com>
2024-08-01 14:28:24 -0400

    initial commit

--- src/test/java/com/revature/TestRunner.java
+++ src/test/java/com/revature/TestRunner.java
@@ -17,7 +17,7 @@ public class TestRunner {
     public static PlanetariumStartPage startPage;
 
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
         startPage = new PlanetariumStartPage(driver);
@@ -25,7 +25,7 @@ public class TestRunner {
     }
 
     @AfterClass
-    public static void teardown(){
+    public static void teardown() {
         driver.quit();
     }
 
@@ -35,5 +35,10 @@ public class TestRunner {
         startPage.goToStartPage();
         System.out.println(driver.getTitle());
         teardown();
+    }
+
+    private static class WebDriverWait {
+        public WebDriverWait(WebDriver driver, Duration duration) {
+        }
     }
 }
